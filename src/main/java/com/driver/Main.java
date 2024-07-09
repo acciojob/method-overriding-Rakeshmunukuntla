5c0bdc8b5835b4b4f46a5e1f7d4663ba752d472d
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2024-07-09 17:21:43 +0530

    tried

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,3 +8,15 @@ public class Main
         b.meth();
     }
 }
+class A {
+    public String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}

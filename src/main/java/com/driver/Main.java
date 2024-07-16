c66b189f87ce8157ebe8a2eb8be00b297406eca8
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2024-07-16 13:58:11 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,20 +4,10 @@ public class Main
 {
     public static void main(String[] args)
     {
+       A a=new A();
+        System.out.println(a.meth());
         B b=new B();
-        //b.meth();
-        b.meth();
+        System.out.println(b.meth());
     }
 }
-class A {
-    public String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
+

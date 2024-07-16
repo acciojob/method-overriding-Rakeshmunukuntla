4ed92aab8b4e7e95f71a8998fae42fa20fd45396
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2024-07-16 14:14:06 +0530

    ass done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-       A a=new A();
+        B a= (B) new A();
         System.out.println(a.meth());
         B b=new B();
         System.out.println(b.meth());
